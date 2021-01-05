b29899dbe026416cefae2b760b6f6549f2e1c57d
dwax1324 <dwax1324@gmail.com>
2021-01-05 17:14:49 +0900

    update

---
+++ kksqwe.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main(){
+
+    int p1,s1,p2,s2;
+    cin >> p1 >> s1 >> s2 >> p2;
+
+    if(p1+p2 > s1+ s2){
+        cout << "Persepolis";
+    }else if(p1+p2 < s1+ s2){
+        cout << "Esteghlal";
+    }else{
+        if(s1 > p2){
+            cout << "Esteghlal";
+        }else if( s1 < p2){
+            cout << "Persepolis";
+        }else{
+            cout << "Penalty";
+        }
+
+    }
+
+
+}
